55474c111a3f926242a1912cde738adf1d66754f
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2019-10-10 12:14:00 +0200

    Comitting profile page

---
+++ app/src/main/java/com/example/healthy/ProfilePage.java
@@ -0,0 +1,15 @@
+package com.example.healthy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_page);
+
+    }
+}
