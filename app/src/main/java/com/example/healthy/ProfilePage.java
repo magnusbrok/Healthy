662bf41c7e3e8de50bae5f3a76bcdbb5325af14c
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2019-10-10 12:20:49 +0200

    ProfilePage

--- app/src/main/java/com/example/healthy/ProfilePage.java
+++ app/src/main/java/com/example/healthy/ProfilePage.java
@@ -1,15 +1,14 @@
 package com.example.healthy;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class ProfilePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_social_page);
-
+        setContentView(R.layout.activity_profile_page);
     }
 }
