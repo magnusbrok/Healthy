29d20dec856b417733f3a04704cdc5a7b823ab04
Siff <siff.ravn@gmail.com>
2019-10-28 13:13:49 +0100

    TimeMenu implemented and added to defaults frame in ActivityPage

--- app/src/main/java/com/example/healthy/Activity/TimeMenu.java
+++ app/src/main/java/com/example/healthy/Activity/TimeMenu.java
@@ -9,16 +9,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.healthy.R;
 
-public class TimeMenu extends Fragment {
+public class TimeMenu extends Fragment implements View.OnClickListener {
+
+    TextView day, week, month;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_day_activities, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_menu, container, false);
+        day = root.findViewById(R.id.day);
+        week = root.findViewById(R.id.week);
+        month = root.findViewById(R.id.month);
+
+        //set
+        day.setOnClickListener(this);
+        week.setOnClickListener(this);
+        month.setOnClickListener(this);
+
+        return root;
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
