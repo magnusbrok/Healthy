470a0c2a69cf0036bb00df18af53c8120027cd36
Siff <siff.ravn@gmail.com>
2019-10-28 13:04:07 +0100

    Added TimeMenu fragment to project

---
+++ app/src/main/java/com/example/healthy/Activity/TimeMenu.java
@@ -0,0 +1,23 @@
+package com.example.healthy.Activity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.healthy.R;
+
+public class TimeMenu extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time_menu, container, false);
+    }
+}
