0c669acf39fa63ac0b05290d694fc2a3949b95a3
Siff <siff.ravn@gmail.com>
2019-10-28 13:26:13 +0100

    Update TimeMenu
    Fragment navigation implemented on click

--- app/src/main/java/com/example/healthy/Activity/TimeMenu.java
+++ app/src/main/java/com/example/healthy/Activity/TimeMenu.java
@@ -1,6 +1,7 @@
 package com.example.healthy.Activity;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.healthy.Nutrition.Week;
 import com.example.healthy.R;
 
 public class TimeMenu extends Fragment implements View.OnClickListener {
@@ -37,6 +39,29 @@ public class TimeMenu extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        if (v == day){
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.FrameLayout, new DayActivities())
+                    .commit();
+            day.setTypeface(null,Typeface.BOLD);
+            week.setTypeface(null,Typeface.NORMAL);
+            month.setTypeface(null,Typeface.NORMAL);
 
+        }else if (v == week){
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.FrameLayout, new WeekActivities())
+                    .commit();
+            day.setTypeface(null,Typeface.NORMAL);
+            week.setTypeface(null,Typeface.BOLD);
+            month.setTypeface(null,Typeface.NORMAL);
+
+        }else if (v == month){
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.FrameLayout, new MonthActivities())
+                    .commit();
+            day.setTypeface(null,Typeface.NORMAL);
+            week.setTypeface(null,Typeface.NORMAL);
+            month.setTypeface(null,Typeface.BOLD);
+        }
     }
 }
