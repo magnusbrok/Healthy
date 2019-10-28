8e323f79afdce1c3ad3a2f8c491c2a56badb89cd
Siff <siff.ravn@gmail.com>
2019-10-28 12:13:58 +0100

    Added Week- and MonthActivities fragments to project

---
+++ app/src/main/java/com/example/healthy/Activity/WeekActivities.java
@@ -0,0 +1,27 @@
+package com.example.healthy.Activity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.healthy.R;
+
+public class WeekActivities extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_week_activities, container, false);
+
+        // Inflate the layout for this fragment
+
+        return root;
+    }
+
+}
