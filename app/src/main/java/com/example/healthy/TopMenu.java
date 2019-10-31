6c3b45361e569c0586d7223d91563d1be0a77397
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2019-10-31 16:50:06 +0100

    Implementing settings- and profile pages

--- app/src/main/java/com/example/healthy/TopMenu.java
+++ app/src/main/java/com/example/healthy/TopMenu.java
@@ -1,6 +1,8 @@
 package com.example.healthy;
 
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,24 +10,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class TopMenu extends Fragment implements View.OnClickListener {
 
-
-    public TopMenu() {
-        // Required empty public constructor
-    }
+    ImageView profileTab, settingsTab;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_menu, container, false);
+        View root = inflater.inflate(R.layout.fragment_top_menu, container, false);
+
+        profileTab = root.findViewById(R.id.profileTab);
+        settingsTab = root.findViewById(R.id.settingsTab);
+        profileTab.setOnClickListener(this);
+        settingsTab.setOnClickListener(this);
+
+        return root;
     }
 
     @Override
     public void onClick(View v) {
+        if (v == profileTab) {
+            Intent intent = new Intent(getActivity(), ProfilePage.class);
+            startActivity(intent);
+        } else if (v == settingsTab) {
+            Intent intent = new Intent(getActivity(), SettingsPage.class);
+            startActivity(intent);
+        }
     }
 }
