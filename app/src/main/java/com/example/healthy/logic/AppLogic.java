3da5038f34754feb57a2df9240459afe2bc9f908
Siff <siff.ravn@gmail.com>
2019-11-18 17:09:01 +0100

    steps getters and setters implemented in Activity points and AppLogic

--- app/src/main/java/com/example/healthy/logic/AppLogic.java
+++ app/src/main/java/com/example/healthy/logic/AppLogic.java
@@ -1,4 +1,15 @@
 package com.example.healthy.logic;
 
 public class AppLogic {
+        private ActivityPoints activityPoints = new ActivityPoints();
+
+        public int getSteps(){
+            return activityPoints.getSteps();
+        }
+
+        public void setSteps(int steps){
+            activityPoints.setSteps(steps);
+        }
+
+
 }
