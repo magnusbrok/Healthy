d4212289288e3bf26f49f3287f2602d72ff90f9a
Siff <siff.ravn@gmail.com>
2019-11-18 22:27:33 +0100

    Revert "ObserverPattern intgrated in DayActivity and LogicPackage"
    This reverts commit 02965973bf59858d8b1923f72338d22a10b318cc.

--- app/src/main/java/com/example/healthy/logic/AppLogic.java
+++ app/src/main/java/com/example/healthy/logic/AppLogic.java
@@ -1,7 +1,5 @@
 package com.example.healthy.logic;
 
-import com.example.healthy.ObserverPattern.Observer;
-
 public class AppLogic {
         private ActivityPoints activityPoints = new ActivityPoints();
 
@@ -17,10 +15,6 @@ public class AppLogic {
 
         public void setSteps(int steps){
             activityPoints.setSteps(steps);
-        }
-
-        public void attachObserverToActivityPoints(Observer observer){
-            activityPoints.attachObserver(observer);
         }
 
 
