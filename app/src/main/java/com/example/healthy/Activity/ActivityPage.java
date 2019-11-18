45c501c65e2372db88603ad06d92ee345d2e3653
Siff <siff.ravn@gmail.com>
2019-11-18 16:20:25 +0100

    eventListener registered for stepCounter sensor in Activity

--- app/src/main/java/com/example/healthy/Activity/ActivityPage.java
+++ app/src/main/java/com/example/healthy/Activity/ActivityPage.java
@@ -2,13 +2,20 @@ package com.example.healthy.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.example.healthy.BottomMenu;
 import com.example.healthy.R;
 import com.example.healthy.TopMenu;
+import static android.hardware.Sensor.TYPE_STEP_COUNTER;
 
 public class ActivityPage extends AppCompatActivity {
+
+    SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    Sensor stepCounter = sensorManager.getDefaultSensor(TYPE_STEP_COUNTER);
+    StepCountListener stepCountListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +39,7 @@ public class ActivityPage extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().add(R.id.TopFrame, topFragment).commit();
             }
         }
+
+        sensorManager.registerListener(stepCountListener, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
