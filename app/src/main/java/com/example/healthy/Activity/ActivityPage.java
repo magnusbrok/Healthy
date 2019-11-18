713c5db0d98207d1b1e8196a632ad7c5600d1b96
Siff <siff.ravn@gmail.com>
2019-11-18 17:38:43 +0100

    onSensorChanged implemented in Activity

--- app/src/main/java/com/example/healthy/Activity/ActivityPage.java
+++ app/src/main/java/com/example/healthy/Activity/ActivityPage.java
@@ -3,19 +3,23 @@ package com.example.healthy.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.example.healthy.BottomMenu;
 import com.example.healthy.R;
 import com.example.healthy.TopMenu;
+import com.example.healthy.logic.AppLogic;
+
 import static android.hardware.Sensor.TYPE_STEP_COUNTER;
 
-public class ActivityPage extends AppCompatActivity {
+public class ActivityPage extends AppCompatActivity implements SensorEventListener {
 
     SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     Sensor stepCounter = sensorManager.getDefaultSensor(TYPE_STEP_COUNTER);
-    StepCountListener stepCountListener;
+    AppLogic appLogic = AppLogic.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,16 @@ public class ActivityPage extends AppCompatActivity {
             }
         }
 
-        sensorManager.registerListener(stepCountListener, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        appLogic.setSteps((int) event.values[0]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
