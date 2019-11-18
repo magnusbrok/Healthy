7ba669f8b188b30497b937e2decaa291e504cae9
Siff <siff.ravn@gmail.com>
2019-11-18 17:14:11 +0100

    singleton pattern applid to AppLogic

--- app/src/main/java/com/example/healthy/logic/AppLogic.java
+++ app/src/main/java/com/example/healthy/logic/AppLogic.java
@@ -3,6 +3,12 @@ package com.example.healthy.logic;
 public class AppLogic {
         private ActivityPoints activityPoints = new ActivityPoints();
 
+        private static AppLogic instance = new AppLogic();
+
+        public static AppLogic getInstance(){
+            return instance;
+        }
+
         public int getSteps(){
             return activityPoints.getSteps();
         }
