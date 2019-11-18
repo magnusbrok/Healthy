02965973bf59858d8b1923f72338d22a10b318cc
Siff <siff.ravn@gmail.com>
2019-11-18 22:27:04 +0100

    ObserverPattern intgrated in DayActivity and LogicPackage

--- app/src/main/java/com/example/healthy/logic/AppLogic.java
+++ app/src/main/java/com/example/healthy/logic/AppLogic.java
@@ -1,5 +1,7 @@
 package com.example.healthy.logic;
 
+import com.example.healthy.ObserverPattern.Observer;
+
 public class AppLogic {
         private ActivityPoints activityPoints = new ActivityPoints();
 
@@ -15,6 +17,10 @@ public class AppLogic {
 
         public void setSteps(int steps){
             activityPoints.setSteps(steps);
+        }
+
+        public void attachObserverToActivityPoints(Observer observer){
+            activityPoints.attachObserver(observer);
         }
 
 
