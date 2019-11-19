0ff988b681dd20ddceade98e0412ea1f523dd853
Siff <siff.ravn@gmail.com>
2019-11-19 15:02:44 +0100

    computePoints implemented in ActivityPoints

--- app/src/main/java/com/example/healthy/logic/ActivityPoints.java
+++ app/src/main/java/com/example/healthy/logic/ActivityPoints.java
@@ -4,9 +4,28 @@ public class ActivityPoints extends Points {
 
     private int points;
     private int steps;
+    private int[] stepGoals = new int[6];
+
+    public ActivityPoints(){
+        // Generate step milestones with intervals of 2500
+        for (int i = 0; i < stepGoals.length; i++){
+            stepGoals[i] = 2500*(i+1);
+        }
+    }
 
     public void computePoints(){
         //TODO implement code to compute points from steps taken
+
+        int points = 0;
+
+        //Increments local points if steps taken are exceeds an milestone
+        for (int i = 0; i < stepGoals.length; i++){
+            if (stepGoals[i] < steps){
+                points += 50;
+            }
+        }
+
+        setPoints(points);
     }
 
     //getters and setters
@@ -16,6 +35,7 @@ public class ActivityPoints extends Points {
 
     public void setPoints(int points) {
         this.points = points;
+        notifyChangeToObservers();
     }
 
     public int getSteps() {
