c388ceb347e7d8c165205516541a557959830eb0
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2019-11-19 16:17:07 +0100

    Adding PopUp dialog in profile page

--- app/src/main/java/com/example/healthy/ProfilePage.java
+++ app/src/main/java/com/example/healthy/ProfilePage.java
@@ -2,19 +2,28 @@ package com.example.healthy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfilePage extends AppCompatActivity implements View.OnClickListener {
 
     ImageView settingsTab;
+    TextView
+    Button editProfile;
+    Dialog profileDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        profileDialog = new Dialog (this);
 
         settingsTab = findViewById(R.id.settingsTab);
         settingsTab.setOnClickListener(this);
@@ -23,6 +32,21 @@ public class ProfilePage extends AppCompatActivity implements View.OnClickListen
             final BottomMenu fragment = new BottomMenu();
             getSupportFragmentManager().beginTransaction().add(R.id.menuFragment, fragment).commit();
         }
+    }
+    public void showPopUp (View v) {
+        TextView lukPopUp;
+        EditText redigerNavn, redigerEmail, redigerArgang, redigerAlder, redigerSkole;
+        Button gemRedigeringer;
+        profileDialog.setContentView(R.layout.activity_edit_profile_page);
+        lukPopUp = profileDialog.findViewById(R.id.lukPopUp);
+        gemRedigeringer = profileDialog.findViewById(R.id.gemRedigering);
+        lukPopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            profileDialog.dismiss();
+            }
+        });
+        profileDialog.show();
     }
 
     @Override
