c1fb890486ee2084e782bb4b6e8a4f8746d98a80
Helle <helle1926@hotmail.com>
2019-11-19 21:53:21 +0100

    scrollview + prizepage i reward

--- app/src/main/java/com/example/healthy/Reward/RewardPage.java
+++ app/src/main/java/com/example/healthy/Reward/RewardPage.java
@@ -2,13 +2,18 @@ package com.example.healthy.Reward;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.healthy.BottomMenu;
 import com.example.healthy.R;
 import com.example.healthy.TopMenu;
 
-public class RewardPage extends AppCompatActivity {
+
+public class RewardPage extends AppCompatActivity implements View.OnClickListener {
+    Button seGevinster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,14 @@ public class RewardPage extends AppCompatActivity {
             final TopMenu fragment = new TopMenu();
             getSupportFragmentManager().beginTransaction().add(R.id.topMenuFragment, fragment).commit();
         }
+        seGevinster = findViewById(R.id.showRewardButton);
+        seGevinster.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        if (v == seGevinster){
+        Intent intent = new Intent(this, YourPrizePage.class);
+        startActivity(intent);}
     }
 }
---
+++ app/src/main/java/com/example/healthy/Reward/YourPrizePage.java
@@ -0,0 +1,16 @@
+package com.example.healthy.Reward;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthy.R;
+
+public class YourPrizePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_prize_page);
+    }
+}
