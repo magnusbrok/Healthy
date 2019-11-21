0d6f7af8c65e94cc2198a7ecd06ec7a91e253414
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2019-11-21 10:27:59 +0100

    Deleting un-used imports

--- app/src/main/java/com/example/healthy/ProfilePage.java
+++ app/src/main/java/com/example/healthy/ProfilePage.java
@@ -1,17 +1,11 @@
 package com.example.healthy;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
