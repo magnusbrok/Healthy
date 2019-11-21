378a5ed9b49e84710b4b2e5db03f58aa76e1b642
Anders Brandt <s185016@student.dtu.dk>
2019-11-21 12:17:05 +0100

    RewardPage and YourPrizePage are now fragments

--- app/src/main/java/com/example/healthy/Reward/RewardPageFragment.java
+++ app/src/main/java/com/example/healthy/Reward/RewardPageFragment.java
@@ -1,20 +1,24 @@
 package com.example.healthy.Reward;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.healthy.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RewardPageFragment extends Fragment {
+public class RewardPageFragment extends Fragment implements View.OnClickListener {
+    Button seGevinster;
 
 
     public RewardPageFragment() {
@@ -27,7 +31,20 @@ public class RewardPageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_reward_page, container, false);
+
+        seGevinster = root.findViewById(R.id.showRewardButton);
+        seGevinster.setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == seGevinster) {
+            Fragment yourPrizes = new YourPrizePageFragment();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragmentView, yourPrizes);
+            transaction.commit();
+            }
     }
 
 }
