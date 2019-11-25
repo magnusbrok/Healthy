959aac9194fa5bb208b095ce15729a7d3a20fc22
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2019-11-25 12:17:00 +0100

    Update SettingsPage.java

--- app/src/main/java/com/example/healthy/SettingsPage.java
+++ app/src/main/java/com/example/healthy/SettingsPage.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SettingsPage extends AppCompatActivity implements View.OnClickListener {
 
     ImageView profileTab;
+    Button backToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,10 @@ public class SettingsPage extends AppCompatActivity implements View.OnClickListe
 
         profileTab = findViewById(R.id.profileTab);
         profileTab.setOnClickListener(this);
+        backToMain = findViewById(R.id.tilbageKnap2);
+        backToMain.setOnClickListener(this);
 
-        /*if (savedInstanceState == null) {
-            final BottomMenu fragment = new BottomMenu();
-            getSupportFragmentManager().beginTransaction().add(R.id.menuFragment, fragment).commit();
-        }*/
+
     }
 
     @Override
