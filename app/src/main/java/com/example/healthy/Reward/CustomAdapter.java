1f102c0e5f87ce02b672bd4378178a238be5de70
Helle <helle1926@hotmail.com>
2019-11-25 10:52:21 +0100

    farver igen: nutrition og reward

---
+++ app/src/main/java/com/example/healthy/Reward/CustomAdapter.java
@@ -0,0 +1,37 @@
+package com.example.healthy.Reward;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.healthy.R;
+
+
+public class CustomAdapter extends ArrayAdapter<String> {
+
+
+    public CustomAdapter(@NonNull Context context, String[] namesOfPrizes) {
+        super(context, R.layout.fragment_your_prize_page, namesOfPrizes);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater lf = LayoutInflater.from(getContext());
+        View customPrize = lf.inflate(R.layout.activity_your_prize_elements, parent,false);
+
+        String prizeText = getItem(position);
+        TextView singlePrizeText =customPrize.findViewById(R.id.prizeText);
+        ImageView singleImage = customPrize.findViewById(R.id.prizeImage);
+        singlePrizeText.setText(prizeText);
+        singleImage.setImageResource(R.drawable.activity_guy_final);
+        return customPrize;
+    }
+}
+
