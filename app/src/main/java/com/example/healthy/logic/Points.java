9ccb8f6ba64b92127ab391eadc4f42a92a1faaeb
Siff <siff.ravn@gmail.com>
2019-11-25 08:39:24 +0100

    Method override added to Points

--- app/src/main/java/com/example/healthy/logic/Points.java
+++ app/src/main/java/com/example/healthy/logic/Points.java
@@ -4,4 +4,8 @@ import com.example.healthy.ObserverPattern.Subject;
 
 public abstract class Points extends Subject {
 
+    void computePoints(){
+
+    }
+
 }
--- app/src/main/java/com/example/healthy/logic/SocialPoints.java
+++ app/src/main/java/com/example/healthy/logic/SocialPoints.java
@@ -1,6 +1,8 @@
 package com.example.healthy.logic;
 
 public class SocialPoints extends Points {
+
+    @Override
     public void computePoints() {
     }
 }
