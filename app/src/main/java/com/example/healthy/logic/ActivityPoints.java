a76120b5be913b66e7686dbab1f89ef61818fb0c
magnusbrok <magnus.brok@db-data.dk>
2019-12-09 16:37:15 +0100

    Fixed reward point logic

--- app/src/main/java/com/example/healthy/logic/ActivityPoints.java
+++ app/src/main/java/com/example/healthy/logic/ActivityPoints.java
@@ -4,8 +4,10 @@ public class ActivityPoints extends Points {
 
     private int points;
     private int steps;
-    private int[] stepGoals = new int[100];
 
+    private int pointIncrementer = 50;
+
+    private int[] stepGoals = new int[100];
     public ActivityPoints(){
         // Generate step milestones with intervals of 2500
         for (int i = 0; i < stepGoals.length; i++){
@@ -14,26 +16,32 @@ public class ActivityPoints extends Points {
     }
 
     @Override
-    public void computePoints(){
+    public int computePoints(){
         //TODO implement code to compute points from steps taken
+
+        int currentPoints = points;
 
         int points = 0;
 
         //Increments local points if steps taken are exceeds an milestone
         for (int i = 0; i < stepGoals.length; i++){
             if (stepGoals[i] < steps){
-                points += 50;
+                points += pointIncrementer;
             }
         }
 
         setPoints(points);
+
+        int difference = points - currentPoints;
+
+        return (difference);
     }
 
     //getters and setters
+
     public int getPoints() {
         return points;
     }
-
     public void setPoints(int points) {
         this.points = points;
         notifyChangeToObservers();
@@ -46,6 +54,14 @@ public class ActivityPoints extends Points {
     public void setSteps(int steps) {
         this.steps = steps;
         notifyChangeToObservers();
+    }
+
+    public int getPointIncrementer() {
+        return pointIncrementer;
+    }
+
+    public void setPointIncrementer(int pointIncrementer) {
+        this.pointIncrementer = pointIncrementer;
     }
 
 
