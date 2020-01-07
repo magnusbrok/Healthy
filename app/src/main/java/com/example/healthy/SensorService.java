b60313dc389772d951d47e4b143de45fe56da046
Siff <siff.ravn@gmail.com>
2020-01-07 13:04:07 +0100

    SensorService added to project

---
+++ app/src/main/java/com/example/healthy/SensorService.java
@@ -0,0 +1,38 @@
+package com.example.healthy;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.IBinder;
+import android.widget.Toast;
+
+
+public class SensorService extends Service implements SensorEventListener {
+
+    // TODO implement this
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "Service started by user.", Toast.LENGTH_LONG).show();
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
