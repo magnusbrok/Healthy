3682d8a06f38c1e0b90d16af011996385d02d643
Siff <siff.ravn@gmail.com>
2020-01-09 12:09:20 +0100

    stepCount logic inserted

--- app/src/main/java/com/example/healthy/SensorService.java
+++ app/src/main/java/com/example/healthy/SensorService.java
@@ -5,7 +5,9 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,19 +21,29 @@ import androidx.core.app.NotificationCompat;
 
 import com.example.healthy.logic.AppLogic;
 
+import java.util.Calendar;
+
 import static android.hardware.Sensor.TYPE_STEP_COUNTER;
 
 
 public class SensorService extends Service implements SensorEventListener {
 
     // TODO implement this
+    public static final String SHARED_PREFS = "shared_prefs";
+    public static final String HAS_RUN = "has_run";
+    public static final String CALIBRATOR = "calibrator";
+    public static final String LAST_USEDATE = "last_usedate";
+    private boolean unCalibrated;
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     SensorManager sensorManager;
     Sensor stepCounter;
+    SharedPreferences preferences;
+    SharedPreferences.Editor preferenceEditor;
+    Calendar calendar = Calendar.getInstance();
 
     AppLogic appLogic = AppLogic.getInstance();
 
-    public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -40,6 +52,15 @@ public class SensorService extends Service implements SensorEventListener {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        preferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        preferenceEditor = preferences.edit();
+
+        if (!preferences.getBoolean(HAS_RUN,false)){
+            unCalibrated = true;
+            appLogic.setSteps(0);
+            preferenceEditor.putBoolean(HAS_RUN,true).apply();
+        }
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         stepCounter = sensorManager.getDefaultSensor(TYPE_STEP_COUNTER);
@@ -70,10 +91,28 @@ public class SensorService extends Service implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        // Check if steps of the day are uncalibrated
+        // Checks if new day
 
-        int currentSteps = ((int) event.values[0]);
+        if(preferences.getInt(LAST_USEDATE,0) != calendar.get(Calendar.DAY_OF_MONTH)){
+            unCalibrated = true;
+            preferenceEditor.putInt(LAST_USEDATE,calendar.get(Calendar.DAY_OF_MONTH)).apply();
+        }
+        // Checks if phone has rebooted
+        else if ((int) event.values[0] < preferences.getInt(CALIBRATOR,0)){
+            unCalibrated = true;
+        }
+
+        //Calibration of steps of the day
+        if (unCalibrated){
+            preferenceEditor.putInt(CALIBRATOR, (int) event.values[0]).apply();
+            unCalibrated = false;
+        }
+
+        int currentSteps = ((int) event.values[0]) - preferences.getInt(CALIBRATOR,0);
 
         appLogic.setSteps(currentSteps);
+        appLogic.computePoints();
 
     }
 
