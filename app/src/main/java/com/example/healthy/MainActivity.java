2dbd35f237bdc5a6b9dd6057a965acd25ad7b180
Siff <siff.ravn@gmail.com>
2020-01-09 12:11:34 +0100

    code sweep

--- app/src/main/java/com/example/healthy/MainActivity.java
+++ app/src/main/java/com/example/healthy/MainActivity.java
@@ -3,48 +3,24 @@ package com.example.healthy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.example.healthy.Activity.ActivityPageFragment;
 import com.example.healthy.Nutrition.NutritionPageFragment;
 import com.example.healthy.Reward.RewardPageFragment;
 import com.example.healthy.Social.SocialPageFragment;
-import com.example.healthy.logic.AppLogic;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.Calendar;
-
-import static android.hardware.Sensor.TYPE_STEP_COUNTER;
 
 public class MainActivity extends AppCompatActivity {
 
-//    SensorManager sensorManager;
-//    Sensor stepCounter;
-//    FrameLayout topMenuView;
     BottomNavigationView bottomMenu;
-//    AppLogic appLogic = AppLogic.getInstance();
-//    Calendar calendar = Calendar.getInstance();
-//
     public static final String SHARED_PREFS = "shared_prefs";
-//    public static final String HAS_RUN = "has_run";
-//    public static final String CALIBRATOR = "calibrator";
-//    public static final String LAST_USEDATE = "last_usedate";
-//    private boolean unCalibrated;
-
-//    SharedPreferences preferences;
-//    SharedPreferences.Editor preferenceEditor;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -52,23 +28,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_menu);
 
         startService(new Intent(this, SensorService.class));
-
-//        preferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-//        preferenceEditor = preferences.edit();
-//
-//        if (!preferences.getBoolean(HAS_RUN,false)){
-//            unCalibrated = true;
-//            appLogic.setSteps(0);
-//            preferenceEditor.putBoolean(HAS_RUN,true).apply();
-//        }
-//
-//        if(preferences.getInt(LAST_USEDATE,0) == 0){
-//            preferenceEditor.putInt(LAST_USEDATE,calendar.get(Calendar.DAY_OF_MONTH)).apply();
-//        }
-//
-//        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-//        stepCounter = sensorManager.getDefaultSensor(TYPE_STEP_COUNTER);
-//        sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
 
         if (savedInstanceState == null) {
             final HomePageFragment fragment = new HomePageFragment();
@@ -140,38 +99,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-//    @Override
-//    public void onSensorChanged(SensorEvent event) {
-//        //TODO implement code to set steps taken for current day
-//
-//        // Check if steps of the day are uncalibrated
-//        // Checks if new day
-//        if(preferences.getInt(LAST_USEDATE,0) != calendar.get(Calendar.DAY_OF_MONTH)){
-//            unCalibrated = true;
-//            preferenceEditor.putInt(LAST_USEDATE,calendar.get(Calendar.DAY_OF_MONTH)).apply();
-//        }
-//        // Checks if phone has rebooted
-//        else if ((int) event.values[0] < preferences.getInt(CALIBRATOR,0)){
-//            unCalibrated = true;
-//        }
-//
-//        //Calibration of steps of the day
-//        if (unCalibrated){
-//            preferenceEditor.putInt(CALIBRATOR, (int) event.values[0]).apply();
-//            unCalibrated = false;
-//        }
-//
-//        int currentSteps = ((int) event.values[0]) - preferences.getInt(CALIBRATOR,0);
-//
-//        appLogic.setSteps(currentSteps);
-//        appLogic.computePoints();
-//    }
-//
-//    @Override
-//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//
-//    }
-//
     public void changeMenu(int itemId) {
         bottomMenu = findViewById(R.id.bottom_navigation);
         bottomMenu.setSelectedItemId(itemId);
