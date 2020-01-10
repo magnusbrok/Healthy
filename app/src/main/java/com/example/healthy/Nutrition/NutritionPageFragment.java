efcaf8f62fec33175501b61f917c416505038ee6
Siff <siff.ravn@gmail.com>
2020-01-10 11:40:39 +0100

    Inflates nutrition page from same layout as activity page

--- app/src/main/java/com/example/healthy/Nutrition/NutritionPageFragment.java
+++ app/src/main/java/com/example/healthy/Nutrition/NutritionPageFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.healthy.Activity.TimeMenu;
 import com.example.healthy.R;
 
 /**
@@ -21,71 +22,51 @@ import com.example.healthy.R;
  */
 public class NutritionPageFragment extends Fragment implements View.OnClickListener {
 
-    TextView day, week, month, goal, log;
-    ImageButton buttonPlus;
+//    TextView day, week, month, goal, log;
+//    ImageButton buttonPlus;
 
     FragmentTransaction fragmentTransaction;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_nutrition_page, container, false);
+        View root = inflater.inflate(R.layout.fragment_activity_page, container, false);
 
-        day = root.findViewById(R.id.meat);
-        day.setOnClickListener(this);
-        week = root.findViewById(R.id.fish);
-        week.setOnClickListener(this);
-        month = root.findViewById(R.id.wholeMeal);
-        month.setOnClickListener(this);
-        goal = root.findViewById(R.id.dairy);
-        goal.setOnClickListener(this);
-        buttonPlus = root.findViewById(R.id.water);
-        buttonPlus.setOnClickListener(this);
-        log = root.findViewById(R.id.beverages);
-        log.setOnClickListener(this);
+//        day = root.findViewById(R.id.meat);
+//        day.setOnClickListener(this);
+//        week = root.findViewById(R.id.fish);
+//        week.setOnClickListener(this);
+//        month = root.findViewById(R.id.wholeMeal);
+//        month.setOnClickListener(this);
+//        goal = root.findViewById(R.id.dairy);
+//        goal.setOnClickListener(this);
+//        buttonPlus = root.findViewById(R.id.water);
+//        buttonPlus.setOnClickListener(this);
+//        log = root.findViewById(R.id.beverages);
+//        log.setOnClickListener(this);
 
         if(savedInstanceState == null) {
             fragmentTransaction = getFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.frameLayoutNutrition,new Day());
+            fragmentTransaction.replace(R.id.FrameLayout ,new Day());
             fragmentTransaction.commit();
+
+            fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.TimeFrame, new TimeMenu())
+                    .commit();
         }
         return root;
     }
         public void onClick(View view) {
-            if (view == day){
-                day.setTypeface(null, Typeface.BOLD);
-                week.setTypeface(null,Typeface.NORMAL);
-                month.setTypeface(null,Typeface.NORMAL);
-                fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayoutNutrition, new Day())
-                    .commit();
 
-            }
-            if (view == week){
-                day.setTypeface(null,Typeface.NORMAL);
-                week.setTypeface(null, Typeface.BOLD);
-                month.setTypeface(null,Typeface.NORMAL);
-                fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayoutNutrition, new Week())
-                    .commit();
-            }
-            if (view == month){
-                day.setTypeface(null,Typeface.NORMAL);
-                week.setTypeface(null, Typeface.NORMAL);
-                month.setTypeface(null,Typeface.BOLD);
-                fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayoutNutrition, new Month())
-                    .commit();
-            }
-            if (view == buttonPlus){
-                Intent i = new Intent(getActivity(), AddFood.class);
-                startActivity(i);
-            }
-            if (view == log){
-                Intent i = new Intent(getActivity(), FruitsVeggies.class);
-                startActivity(i);
-
-            }
+//            if (view == buttonPlus){
+//                Intent i = new Intent(getActivity(), AddFood.class);
+//                startActivity(i);
+//            }
+//            if (view == log){
+//                Intent i = new Intent(getActivity(), FruitsVeggies.class);
+//                startActivity(i);
+//
+//            }
         }
 
 }
