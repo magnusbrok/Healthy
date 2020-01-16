9f0eca74384e005043e024b28cc77621867af97c
Siff <siff.ravn@gmail.com>
2020-01-16 14:09:06 +0100

    renames xml for nutritionLog and modified constraints

--- app/src/main/java/com/example/healthy/Nutrition/LogHistory.java
+++ app/src/main/java/com/example/healthy/Nutrition/LogHistory.java
@@ -1,11 +1,9 @@
 package com.example.healthy.Nutrition;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,14 +11,8 @@ import android.widget.TextView;
 import com.example.healthy.R;
 import com.example.healthy.Reward.CustomAdapter;
 import com.example.healthy.logic.AppLogic;
-import com.example.healthy.logic.Items.Food;
 import com.example.healthy.logic.Items.*;
-import com.google.android.gms.actions.ItemListIntents;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 
 public class LogHistory extends AppCompatActivity implements View.OnClickListener {
@@ -33,7 +25,7 @@ public class LogHistory extends AppCompatActivity implements View.OnClickListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fruits_veggies);
+        setContentView(R.layout.activity_nutrition_log);
         textView = findViewById(R.id.nutritionSearch);
         textView.setOnClickListener(this);
         doneButton = findViewById(R.id.doneButton);
