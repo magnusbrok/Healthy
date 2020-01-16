cc8816a02f37bbcd4b46b6f5ee32439a6d69649d
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2020-01-16 12:18:37 +0100

    Updating appDAO

--- app/src/main/java/com/example/healthy/logic/AppDAO.java
+++ app/src/main/java/com/example/healthy/logic/AppDAO.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.example.healthy.logic.Items.Reward;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
