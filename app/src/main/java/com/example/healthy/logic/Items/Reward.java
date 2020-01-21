aedf6ceb0f0135442b7039a2e420ad43de003b6a
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2020-01-21 12:50:09 +0100

    Update Reward.java

--- app/src/main/java/com/example/healthy/logic/Items/Reward.java
+++ app/src/main/java/com/example/healthy/logic/Items/Reward.java
@@ -37,7 +37,7 @@ public class Reward extends Item {
             setResID(R.drawable.reward_page_prize_phone);
         }
         if (name.equalsIgnoreCase("basketbold")) {
-            setResID(R.drawable.reward_page_your_prize_basketball);
+            setResID(R.drawable.basketball);
         }
         if (name.equalsIgnoreCase("gavekort")) {
             setResID(R.drawable.reward_page_your_prize_giftcard);
@@ -49,7 +49,52 @@ public class Reward extends Item {
             setResID(R.drawable.reward_page_your_prize_pencil);
         }
         if (name.equalsIgnoreCase("fodbold")) {
-            setResID(R.drawable.reward_page_prize_football);
+            setResID(R.drawable.fodbold);
+        }
+
+
+
+
+
+
+
+        
+        // Opdateret præmie-liste
+        if (name.equalsIgnoreCase("drikkedunk")) {
+            setResID(R.drawable.drikkedunk);
+        }
+        if (name.equalsIgnoreCase("træningselastik")) {
+            setResID(R.drawable.elastik);
+        }
+        if (name.equalsIgnoreCase("elcykel")) {
+            setResID(R.drawable.elcykel);
+        }
+        if (name.equalsIgnoreCase("gavekort")) {
+            setResID(R.drawable.gavekort_bio);
+        }
+        if (name.equalsIgnoreCase("hoodie")) {
+            setResID(R.drawable.hoodie);
+        }
+        if (name.equalsIgnoreCase("iphone holder")) {
+            setResID(R.drawable.iphone_holder);
+        }
+        if (name.equalsIgnoreCase("juice")) {
+            setResID(R.drawable.juice);
+        }
+        if (name.equalsIgnoreCase("10kr til kantine")) {
+            setResID(R.drawable.kantine_10_kr);
+        }
+        if (name.equalsIgnoreCase("løbesko")) {
+            setResID(R.drawable.loebesko);
+        }
+        if (name.equalsIgnoreCase("protein shake")) {
+            setResID(R.drawable.protein_shake);
+        }
+        if (name.equalsIgnoreCase("rawbar")) {
+            setResID(R.drawable.rawbar);
+        }
+        if (name.equalsIgnoreCase("smoothie")) {
+            setResID(R.drawable.smoothie);
         }
     }
 }
